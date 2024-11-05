b2ae2b58e23e0e0780e7646fe0ee29255c34cd06
FIlipNedelchev20 <filnedelchev@gmail.com>
2024-11-05 12:08:55 +0200

    Implemented Search form functionality Second attempt

--- solution.js
+++ solution.js
@@ -30,7 +30,7 @@ function searchFormData(e) {
     const checkIn = data.querySelector('#check-in').value;
     const checkOut = data.querySelector('#check-out').value;
     const people = data.querySelector('#people').value;
-    if (checkIn != '' && checkOut != '' && people != '' && 
+    if (checkIn != '' && checkOut != '' && people != '' && Number(people) < 15 &&
         new Date(checkIn) <= new Date(checkOut)) {
         reservation.startDate = checkIn;
         reservation.endDate = checkOut;
